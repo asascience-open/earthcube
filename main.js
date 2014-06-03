313506d9e951e88621d04b9cb54a277c51a0991d
Charlton Galvarino <charlton@2creek.com>
2014-06-03 17:00:45 -0400

    added map component

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var pageSize = 15;
-var sto;
+var pageSize = 10;
+var searchStore;
+
+var map;
+var searchShadow;
+var searchHilite;
+var proj3857   = new OpenLayers.Projection("EPSG:3857");
+var proj900913 = new OpenLayers.Projection("EPSG:900913");
+var proj4326   = new OpenLayers.Projection("EPSG:4326");
 
 function init() {
-  sto = new Ext.data.Store({
+  searchStore = new Ext.data.Store({
     reader     : new Ext.data.JsonReader({
        idProperty    : 'id'
       ,root          : 'rows'
@@ -18,31 +25,41 @@ function init() {
     ,remoteSort : true
     ,baseParams : {start : 1}
     ,proxy      : new Ext.data.MemoryProxy()
-    ,listeners  : {beforeload : function(sto,opt) {
-      search(Ext.getCmp('gp'),sto,Ext.getCmp('searchText').getValue(),opt.params ? opt.params.start : 1);
-    }}
+    ,listeners  : {
+      beforeload : function(sto,opt) {
+        search(Ext.getCmp('searchPanel'),sto,Ext.getCmp('searchText').getValue(),opt.params ? opt.params.start : 1);
+      }
+      ,load : function(sto) {
+        searchHilite.removeAllFeatures();
+        syncMapWithResults(sto,searchShadow); 
+      }
+      ,clear : function(sto) {
+        searchHilite.removeAllFeatures();
+        syncMapWithResults(sto,searchShadow);
+      }
+    }
   });
 
-  new Ext.Window({
+  var searchPanel = {
      title           : 'Search'
-    ,id              : 'gp'
+    ,id              : 'searchPanel'
     ,layout          : 'fit'
-    ,width           : 640
-    ,height          : 480
+    ,width           : 400
+    ,region          : 'west'
     ,constrainHeader : true
     ,items           : new Ext.grid.GridPanel({
-       store   : sto
+       store   : searchStore
       ,columns : [
         new Ext.grid.RowNumberer({
           renderer : function(v,p,record,rowIndex) {
             if (this.rowspan) {
               p.cellAttr = 'rowspan="'+this.rowspan+'"';
             }
-            return rowIndex + 1 + sto.baseParams.start - 1;
+            return rowIndex + 1 + record.store.baseParams.start - 1;
           }
         })
         ,{id : 'title',dataIndex : 'title',header : 'Description'}
-        ,{id : 'when',dataIndex : 'when',header : 'When',renderer : function(val,p,rec) {
+        ,{id : 'when',dataIndex : 'when',header : 'When',width : 150,renderer : function(val,p,rec) {
           var rows = [];
           _.each(val,function(o) {
             var minT = o.start; 
@@ -61,6 +78,7 @@ function init() {
           });
           return rows.join('<br>');
         }}
+/*
         ,{id : 'where',dataIndex : 'where',header : 'Where',renderer : function(val,p,rec) {
           var rows = [];
           _.each(val,function(o) {
@@ -89,6 +107,7 @@ function init() {
           });
           return rows.join('<br>');
         }}
+*/
       ]
       ,autoExpandColumn : 'title'
       ,border           : false
@@ -108,6 +127,7 @@ function init() {
             this.el.addClass(this.emptyClass);
             this.triggers[0].hide();
             this.hasSearch = false;
+            searchStore.removeAll();
           }
         }
         ,onTrigger2Click : function() {
@@ -116,14 +136,14 @@ function init() {
             this.onTrigger1Click();
             return;
           }
-          sto.load();
+          searchStore.load();
           this.hasSearch = true;
           this.triggers[0].show();
         }
       })]
       ,bbar            : new Ext.PagingToolbar({
          pageSize    : pageSize
-        ,store       : sto
+        ,store       : searchStore
         ,id          : 'searchResultsPagingToolbar'
         ,displayInfo : true
         ,displayMsg  : 'Displaying results {0} - {1} of {2}'
@@ -171,10 +191,55 @@ function init() {
             pages :  total < this.pageSize ? 1 : Math.ceil(total/this.pageSize)
           };
         }
-
       })
+      ,listeners : {
+        mouseover : function(e,t) {
+          var row = this.getView().findRowIndex(t);
+          if (row >= 0) {
+            var rec = this.getStore().getAt(row);
+            if (rec) {
+              searchHilite.removeAllFeatures();
+              searchHilite.addFeatures(makeFeatures(rec));
+              searchHilite.redraw();
+            }
+          }
+        }
+        ,mouseout : function(e,t) {
+          searchHilite.removeAllFeatures();
+          searchHilite.redraw();
+        }
+      }
     })
-  }).show();
+  };
+
+  var mapPanel = {
+     html      : '<div id="map"></div>'
+    ,region    : 'center'
+    ,title     : 'Map'
+    ,listeners : {
+      afterrender : function(p) {
+        initMap();
+      }
+      ,bodyresize : function(p,w,h) {
+        var el = document.getElementById('map');
+        if (el) {
+          el.style.width  = w;
+          el.style.height = h;
+          map.updateSize();
+        }
+      }
+    }
+  };
+
+  new Ext.Viewport({
+     layout : 'border'
+    ,title  : 'Map'
+    ,layout : 'border'
+    ,items  : [ 
+       mapPanel
+      ,searchPanel
+    ]
+  });
 }
 
 function search(cmp,sto,searchText,start) {
@@ -214,6 +279,122 @@ function search(cmp,sto,searchText,start) {
   );
 }
 
+function initMap() {
+  searchShadow = new OpenLayers.Layer.Vector(
+     'Search shadow'
+    ,{styleMap : new OpenLayers.StyleMap({
+      'default' : new OpenLayers.Style(
+        OpenLayers.Util.applyDefaults({
+           fillColor     : '#0000ff'
+          ,fillOpacity   : 0.02
+          ,strokeWidth   : 1
+          ,strokeColor   : '#0000ff'
+          ,strokeOpacity : 0.2
+        })
+      )
+    })}
+  );
+
+  searchHilite = new OpenLayers.Layer.Vector(
+     'Search hilite'
+    ,{styleMap : new OpenLayers.StyleMap({
+      'default' : new OpenLayers.Style(
+        OpenLayers.Util.applyDefaults({
+           fillOpacity   : 0
+          ,strokeWidth   : 2
+          ,strokeColor   : '#ff0000'
+          ,strokeOpacity : 0.75
+          ,label         : "${title}"
+          ,fontColor     : '#000000'
+          ,fontSize      : '13px'
+          ,fontFamily    : 'arial'
+          ,fontWeight    : 'bold'
+        })
+      )
+    })}
+  );
+
+  map = new OpenLayers.Map('map',{
+    layers            : [
+      new OpenLayers.Layer.XYZ(
+         'ESRI Ocean'
+        ,'http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}.jpg'
+        ,{
+           sphericalMercator : true
+          ,isBaseLayer       : true
+          ,wrapDateLine      : true
+        }
+      )
+/*
+      new OpenLayers.Layer.Google('Google Terrain',{
+         type          : google.maps.MapTypeId.TERRAIN
+        ,projection    : proj900913
+      })
+*/
+      ,searchShadow
+      ,searchHilite
+    ]
+    ,controls : [new OpenLayers.Control.Navigation(),new OpenLayers.Control.MousePosition({displayProjection : proj4326})]
+    ,center : new OpenLayers.LonLat(0,0)
+    ,zoom   : 1
+  });
+}
+
+function syncMapWithResults(sto,lyr) {
+  var features = [];
+  sto.each(function(rec) {
+    features = features.concat(makeFeatures(rec));
+  });
+  lyr.removeFeatures(lyr.features);
+  lyr.addFeatures(features);
+  lyr.redraw();
+}
+
+function makeFeatures(rec) {
+  var features = [];
+  _.each(rec.get('where'),function(o) {
+    o.west = o.west <= -179 ? -178 : o.west;
+    o.south = o.south <= -89 ? -88 : o.south;
+    o.east = o.east >= 179 ? 178 : o.east;
+    o.north = o.north >= 89 ? 88 : o.north;
+    var g = {
+       type        : 'Polygon'
+      ,coordinates : [[
+         [o.west,o.south]
+        ,[o.east,o.south]
+        ,[o.east,o.north]
+        ,[o.west,o.north]
+        ,[o.west,o.south]
+      ]]
+    };
+    var geojson = new OpenLayers.Format.GeoJSON();
+    var f       = geojson.read({
+       type     : 'FeatureCollection'
+      ,features : [{
+         type       : 'Feature'
+        ,geometry   : g
+        ,properties : {
+          title : wordwrap(rec.get('title'),20,"\n")
+        }
+      }]
+    });
+    f[0].geometry.transform(proj4326,proj900913);
+/*
+    // change geometry from polygon to line
+    if (f[0].geometry.getArea() == 0) {
+      f[0].geometry = f[0].geometry.getVertices()[0];
+    }
+    else {
+      var vertices = f[0].geometry.getVertices();
+      vertices.push(vertices[0]);
+      f[0].geometry = new OpenLayers.Geometry.LineString(vertices);
+    }
+*/
+    features.push(f[0]);
+  });
+  return features;
+}
+
 function isoDateToDate(s) {
   // 2010-01-01T00:00:00Z
   s = s.replace("\n",'');
@@ -232,4 +413,17 @@ function isoDateToDate(s) {
   else {
     return false;
   }
+}
+
+// http://james.padolsey.com/javascript/wordwrap-for-javascript/
+function wordwrap(str,width,brk,cut) {
+  brk = brk || '\n';
+  width = width || 75;
+  cut = cut || false;
+
+  if (!str) { return str; }
+ 
+  var regex = '.{1,' +width+ '}(\\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\\S+?(\\s|$)');
+ 
+  return str.match( RegExp(regex, 'g') ).join( brk );
 }
