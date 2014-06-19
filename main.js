ae03558c390a0ee45653da3d7971b1ef28d11c6f
bnfish <benjaminfish@gmail.com>
2014-06-19 17:24:17 -0400

    fix 'moodal' typo...

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var searchShadow;
 var searchHilite;
 
 function init() {
-  $('#add-to-map-modal, #download-modal, #link-moodal').modal({show: false});
+  $('#add-to-map-modal, #download-modal, #link-modal').modal({show: false});
   $('#download-modal .btn-primary').on('click',function() {
     createDownloadLink();
   });
